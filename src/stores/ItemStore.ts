65762cc85608b1cfc0f24d0d4772cffe442d491b
Viterkim <viktorkim2005@hotmail.com>
2019-03-21 17:43:04 +0100

    dockerino

--- src/stores/ItemStore.ts
+++ src/stores/ItemStore.ts
@@ -3,7 +3,7 @@ import Item from '../types/Item';
 
 class ItemStore {
   @observable public cartList: Item[] = [];
-  @observable public productList: Item[] = [{id: 1, itemDescription: "Sue", itemName: "Pizzaen", price: 85}, {id: 2, itemDescription: "Den go", itemName: "En goooo pizza", price: 40}];
+  @observable public productList: Item[] = [];
 
   @action
   public addToCart(newItem: Item): void{
